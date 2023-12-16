3f90b37502710480d40d2406c00e2f6336540b8f
Ada Ebling <ada.ebling@gmail.com>
2023-12-16 10:51:17 -0700

    track boolean state of button

--- src/AlikeLogicGrid.js
+++ src/AlikeLogicGrid.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import styles from './AlikeLogicGrid.module.css';
 
 export function AlikeLogicGrid({ elements }) {
+  const [buttonChecked, setButtonChecked] = useState('unchecked');
+
+  const onCheckboxClick = () => {
+    setButtonChecked('checked');
+  }
+
   return (<table>
     <thead>
       <tr>
@@ -14,7 +21,7 @@ export function AlikeLogicGrid({ elements }) {
           <td>{element}</td>
           {Array.from(Array(elements.length - elementIndex), (_el, idx) => {
             return <td key={element.concat(elements[idx])}>
-              <button className={styles.checkboxButton} />
+              <button className={`${styles.checkboxButton} ${styles[buttonChecked]}`} onClick={onCheckboxClick} />
             </td>
           })}
         </tr>)
--- src/AlikeLogicGrid.test.js
+++ src/AlikeLogicGrid.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { AlikeLogicGrid } from './AlikeLogicGrid';
+import userEvent from '@testing-library/user-event';
 
 describe('AlikeLogicGrid', () => {
   it('renders enough buttons', () => {
@@ -8,5 +9,15 @@ describe('AlikeLogicGrid', () => {
     render(<AlikeLogicGrid elements={elements} />);
     const expectedNumber = ((elements.length + 1) * elements.length) / 2;
     expect(screen.queryAllByRole('button').length).toEqual(expectedNumber);
+  });
+
+  it('buttons change class on click', () => {
+    const elements = ['1', '2', '3', '4'];
+
+    render(<AlikeLogicGrid elements={elements} />);
+    const firstButton = screen.getAllByRole('button')[0];
+    expect(firstButton.className.split(/\s+/)).toContain('unchecked');
+    userEvent.click(firstButton);
+    expect(firstButton.className.split(/\s+/)).toContain('checked');
   });
 });
